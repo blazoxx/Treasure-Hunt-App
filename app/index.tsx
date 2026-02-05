7b5e9c32f991de58da8ff9ffed2fcbcdc8877857
Baibhav Pratap Singh <b2p16s19@gmail.com>
2026-02-05 22:07:15 +0530

    chore: update layout and index components with Tailwind CSS styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,33 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Page() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>INDEX IS LOADING</Text>
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     backgroundColor: "#dbeafe",
+//   },
+//   text: {
+//     fontSize: 18,
+//     fontWeight: "bold",
+//     color: "#2563eb",
+//   },
+// });
+
+// export default function Page() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Hello Tailwind</Text>
+//     </View>
+//   );
+// }
+
+const Index = () => {
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#dbeafe'}}>
+      <Text style={{fontSize: 18, color: "blue"}}>Hello Tailwind</Text>
     </View>
   );
 }
+
+export default Index;

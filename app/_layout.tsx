2984f3f35322601aadd1c7b563336fdd9e073c05
Baibhav Pratap Singh <b2p16s19@gmail.com>
2026-02-06 10:49:26 +0530

    feat: implement stack navigation and add login and signup pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,13 @@
-import { Drawer } from "expo-router/drawer";
+import { Stack } from "expo-router";
 import React from "react";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const rootLayout = () => {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
-        <Drawer.Screen name="index" options={{ title: "Home" }} />
-        <Drawer.Screen name="About/index" options={{ title: "About" }} />
-        <Drawer.Screen
-          name="(tab)"
-          options={{ title: "Tabs", headerShown: false }}
-        />
-        <Drawer.Screen
-          name="(drawer)"
-          options={{ drawerItemStyle: { display: "none" } }}
-        />
-      </Drawer>
-    </GestureHandlerRootView>
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="index"/>
+      <Stack.Screen name="login"/>
+      <Stack.Screen name="signup"/>
+    </Stack>
   );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,47 +1,12 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "center",
-//     backgroundColor: "#dbeafe",
-//   },
-//   text: {
-//     fontSize: 18,
-//     fontWeight: "bold",
-//     color: "#2563eb",
-//   },
-// });
-
-// export default function Page() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.text}>Hello Tailwind</Text>
-//     </View>
-//   );
-// }
-
 const Index = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#dbeafe",
-      }}
-    >
-      <Text style={{ fontSize: 18, fontWeight: "bold", color: "blue" }}>
+    <View>
+      <Text style={{ fontSize: 30, fontWeight: "bold", color: "blue" }}>
         Hello theB
       </Text>
-      <Link
-        href={"/about"}
-        style={{ marginTop: 20, fontSize: 16, color: "blue" }}
-      >
-        Go to About page
-      </Link>
     </View>
   );
 };
